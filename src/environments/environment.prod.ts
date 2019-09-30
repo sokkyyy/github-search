8a37aa136158ad2883045aa44dae786af8ba0c60
sokkyyy <ndegwaraymond@gmail.com>
2019-09-30 21:57:48 +0300

    Final Github search

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.github.com/users/',
+  apiKey: '?access_token=2d5e8d1c6cbe1dc8ad0c6ef7458692e5fac1a26a',
 };
